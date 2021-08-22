0a49caed96baf62289dc61c16f0bb011b6e3ce6e
soladitan <sablik2u@yahoo.com>
2021-08-22 13:26:21 +0100

    Add project files.

---
+++ ConsoleAppSnippets/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleAppSnippets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello People!");
+            var x = Console.ReadLine();
+            var y = Console.ReadLine();
+            var z =Convert.ToInt32 (x) + Convert.ToInt32(y);
+            Console.WriteLine("The result is {0}",z);
+            
+        }
+    }
+}
