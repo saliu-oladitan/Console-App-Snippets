ad7aa72694cad6044702ae29d593d82d875d6068
soladitan <sablik2u@yahoo.com>
2021-08-22 18:18:26 +0100

    Editing

--- ConsoleAppSnippets/Program.cs
+++ ConsoleAppSnippets/Program.cs
@@ -1,18 +1,40 @@
 ﻿using System;
 
-
+namespace ConsoleAppSnippetse
+{
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello! Multiply two numbers with this App");
-            Console.Write("Enter the first number = ");
+            Console.WriteLine("Please enter the password.");
             var x = Console.ReadLine();
-            Console.Write("Enter the second number = ");
-            var y = Console.ReadLine();
-            var z =Convert.ToInt32 (x) * Convert.ToInt32(y);
-            Console.WriteLine("The result of multiplying {0} by {1} is {2}",x,y,z);
+            while (x != "Olamilekan")
+            {
+                Console.WriteLine("You are not from Nigeria");
+                Console.WriteLine("What is your name?");
+                x = Console.ReadLine();
+            }
             
         }
     }
+}
 
+
+//-----------------INPUT AND OUTPUT------------------------------------
+// You can use these lines of code. I commented them out here
+//Console.WriteLine("Hello! Multiply two numbers with this App");
+//Console.Write("Enter the first number = ");
+//var x = Console.ReadLine();
+//Console.Write("Enter the second number = ");
+//var y = Console.ReadLine();
+//var z = Convert.ToInt32(x) * Convert.ToInt32(y);
+//Console.WriteLine("The result of multiplying {0} by {1} is {2}", x, y, z);
+
+
+//-----------------IF STATEMENT------------------------------------
+//Console.WriteLine("What is your name?");
+//var x = Console.ReadLine();
+//if (x == "Olamilekan")
+//{
+//    Console.WriteLine("You are from Nigeria");
+//}
