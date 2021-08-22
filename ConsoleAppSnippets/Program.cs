e806c769d07d900e99be48a353d38a652a8fd0aa
soladitan <sablik2u@yahoo.com>
2021-08-22 15:40:18 +0100

    Updated

--- ConsoleAppSnippets/Program.cs
+++ ConsoleAppSnippets/Program.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 
-namespace ConsoleAppSnippets
-{
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello People!");
+            Console.WriteLine("Hello! Multiply two numbers with this App");
+            Console.Write("Enter the first number = ");
             var x = Console.ReadLine();
+            Console.Write("Enter the second number = ");
             var y = Console.ReadLine();
-            var z =Convert.ToInt32 (x) + Convert.ToInt32(y);
-            Console.WriteLine("The result is {0}",z);
+            var z =Convert.ToInt32 (x) * Convert.ToInt32(y);
+            Console.WriteLine("The result of multiplying {0} by {1} is {2}",x,y,z);
             
         }
     }
-}
+
