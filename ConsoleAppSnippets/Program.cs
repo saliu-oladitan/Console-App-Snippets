9f0bf037c27faf0e0ac7d7dbb6ad14ed324790bf
soladitan <sablik2u@yahoo.com>
2021-08-22 18:40:38 +0100

    Editing

--- ConsoleAppSnippets/Program.cs
+++ ConsoleAppSnippets/Program.cs
@@ -6,21 +6,25 @@ namespace ConsoleAppSnippetse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter the password.");
-            var x = Console.ReadLine();
-            while (x != "Olamilekan")
+            var x = 1;
+            while (x < 11)
             {
-                Console.WriteLine("You are not from Nigeria");
-                Console.WriteLine("What is your name?");
-                x = Console.ReadLine();
+                Console.WriteLine(x);
+                x++;
             }
+
             
+            for(int i=10; i>0; i--)
+            {
+                Console.WriteLine(i);
+            }
+
         }
     }
 }
 
 
-//-----------------INPUT AND OUTPUT------------------------------------
+//1.-----------------INPUT AND OUTPUT------------------------------------
 // You can use these lines of code. I commented them out here
 //Console.WriteLine("Hello! Multiply two numbers with this App");
 //Console.Write("Enter the first number = ");
@@ -31,10 +35,35 @@ namespace ConsoleAppSnippetse
 //Console.WriteLine("The result of multiplying {0} by {1} is {2}", x, y, z);
 
 
-//-----------------IF STATEMENT------------------------------------
+//2.-----------------CHECK THE SIZE OF A DATA TYPE (RESULT IS GIVEN IN BYTES => 1 Byte = 8 bits)----------
+
+//Console.WriteLine("The size of int is {0}", sizeof(int));
+
+
+
+//3.-----------------IF STATEMENT----------------------------------------------------------------
 //Console.WriteLine("What is your name?");
 //var x = Console.ReadLine();
 //if (x == "Olamilekan")
 //{
 //    Console.WriteLine("You are from Nigeria");
 //}
+
+
+
+//4.
+//----------------WHILE LOOPS-------------------------------------
+//var x = "";
+//while (x != "123")
+//{
+
+//    Console.WriteLine("Please enter the password.");
+//    Console.WriteLine("");
+//    x = Console.ReadLine();
+//    if (x != "123")
+//    {
+//        Console.WriteLine("Error, You are Not Authorized");
+//    }
+//}
+
+//Console.WriteLine("Welcome, You are authorized");
